cfc98a046c9bb243385c6dad45f9ce468a920059
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-04-03 15:33:54 +0100

    movies props populated

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import Movie from './components/Movie'
 
 const FEATURED_API =
   'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1'
-
-const IMG_API = 'https://image.tmdb.org/t/p/w1280'
-
 const SEARCH_API =
   'https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query='
 
@@ -23,7 +20,7 @@ function App() {
   return (
     <div>
       {movies.map((movie) => (
-        <Movie key={movie.id} data={movie} />
+        <Movie key={movie.id} {...movie} />
       ))}
     </div>
   )
